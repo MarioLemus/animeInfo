e4636557305527dd33541cebc95885efa4d4eb55
Mario Lemus <theliarnorland@gmail.com>
2021-07-06 17:49:15 -0600

    re-edicion de codigo y validaciones parte 1

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -29,23 +29,59 @@ const P_TITLE = styled.p`
 const DetailView = () => {
     const {currentAnimeShows} = useParams()
     const {animeShowId} = useParams()
-    const [detailData, setDetailData] = useState(0);
+    const [detailData, setDetailData] = useState();
     const url = `https://kitsu.io/api/edge/anime/${animeShowId}`;
+    
+    const finalData = {
+        hasTitle: "" || 'No Title Loaded...',
+        hasSynopsis: "" || 'No Synopsis Loaded...',
+        hasFinished: "" || 'No data',
+        hasEpisodeCount: "" || 'No data',
+        hasStatus: "" || 'No data'
+    };
+    const [animeShow, setAnimeShow] = useState(finalData);
 
     useEffect(() => {
-        const fetchingAnimeData = async () => {
-            const res = await  fetch( url );
-            const { data } = await res.json();
-            console.log( data )
-            setDetailData( data );
+        const fetchingAnimeData = async (animeUrl) => {
+            try {
+                const res = await  fetch(animeUrl);
+                const {data} = await res.json();
+                console.log(data);
+                setDetailData(data);
+            } 
+
+            catch (error) {
+                console.log('No info was fetch check your internet connection');
+            }
         }
-        fetchingAnimeData();
+        fetchingAnimeData(url);
     }, [url])
     
-	console.log(detailData);
-    console.log(`actual id de 2: ${animeShowId}`)
-    console.log(`actual id de 1: ${currentAnimeShows}`)
+	// console.log(detailData);
+    // console.log(`actual id de 2: ${animeShowId}`)
+    // console.log(`actual id de 1: ${currentAnimeShows}`)
+    
+    useEffect(() => {
+        const HandleViewData = () => {
+            if (detailData) {
+                try {
+                    setAnimeShow({
+                        hasTitle: detailData.attributes.canonicalTitle,
+                        hasSynopsis: detailData.attributes.description,
+                        hasFinished: detailData.attributes.endDate,
+                        hasEpisodeCount: detailData.attributes.episodeCount,
+                        hasStatus: detailData.attributes.status
+                    })
 
+                } catch (e) {
+                    console.log('Currently undefined data loading will take some seconds');
+                }  
+            }
+        }    
+        HandleViewData()
+    }, [detailData])
+    
+    
     return (
         <div>
             <Navbar />
@@ -55,8 +91,11 @@ const DetailView = () => {
             <DIV_CONTAINER>
                 {!detailData ? (<p>LOADING...</p>) : (
                     <div>
-                        <P_TITLE>{detailData.attributes.canonicalTitle}</P_TITLE>
-                        <PRE_SYNOPSIS>{detailData.attributes.description}</PRE_SYNOPSIS>
+                        {animeShow.hasTitle === null ? '' : <P_TITLE>{animeShow.hasTitle}</P_TITLE>}
+                        {animeShow.hasSynopsis === null ? '' : <PRE_SYNOPSIS>{animeShow.hasSynopsis}</PRE_SYNOPSIS>}
+                        {animeShow.hasFinished === null ? '' : <div><p>Endede at: {animeShow.hasFinished}</p></div>}
+                        {animeShow.hasEpisodeCount === null ? '' : <div><p>Episode count: {animeShow.hasEpisodeCount}</p></div>}
+                        {animeShow.hasStatus === null ? '' : <div><p>Status: {animeShow.hasStatus}</p></div>}
                     </div>
                 )}
             </DIV_CONTAINER>   
